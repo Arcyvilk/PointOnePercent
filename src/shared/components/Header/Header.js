b8c5c3c55edfa93b3a3a5d1af53eeb5215331b54
Arcyvilk <blazemakerrr@gmail.com>
2018-12-03 12:21:03 +0100

    Reenabling Login modal

--- src/shared/components/Header/Header.js
+++ src/shared/components/Header/Header.js
@@ -1,13 +1,13 @@
 import React from 'react'
-// import Login from '../Login'
+import Login from '../Login'
 
 export default class Header extends React.Component{
     render() {
         return (
             <div className='header flex-row'>
-                {/* <i className='fab fa-steam' /> */}
+                <i className='fab fa-steam' />
                 <p className='header-motto'>Games that masochists love.</p>
-                {/* <Login /> */}
+                <Login />
             </div>
         )
     }
